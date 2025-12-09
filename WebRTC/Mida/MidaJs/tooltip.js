53f5a46891e952f1b0f12142ba760cdfe4cb64fe
animeroman <163325219+animeroman@users.noreply.github.com>
2025-12-09 20:19:11 +0400

    deleting mida completely

--- WebRTC/Mida/MidaJs/tooltip.js
+++
@@ -1,9 +0,0 @@
-function toggleTooltip(id, isVisible) {
-  var element = document.getElementById(id + '-tooltip');
-
-  if (isVisible) {
-    element.classList.remove('d-none');
-  } else {
-    element.classList.add('d-none');
-  }
-}
